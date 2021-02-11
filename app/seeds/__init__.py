400c4139dbf1a23bcd9891d3a555d8b4a639209e
jimmy <demeatricej.sherrod@gmail.com>
2021-02-11 10:12:39 -0600

    imported seeds t init.py for seeds

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,5 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
+from .dreams import seed_dreams, undo_dreams
+from .fragments import seed_fragments, undo_fragments
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -9,10 +11,14 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     seed_users()
+    seed_dreams()
+    seed_fragments()
     # Add other seed functions here
 
 # Creates the `flask seed undo` command
 @seed_commands.command('undo')
 def undo():
     undo_users()
+    undo_dreams()
+    undo_fragments()
     # Add other undo functions here
