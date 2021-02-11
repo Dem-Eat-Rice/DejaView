d684fc4364d36d293e846db97573bad738823fff
jimmy <demeatricej.sherrod@gmail.com>
2021-02-11 11:06:46 -0600

    building up store

--- react-app/src/store/users.js
+++ react-app/src/store/users.js
@@ -1,3 +1,20 @@
 import reducer from "./dreams";
 
+const SET_SINGLE_USER = "SET_SINGLE_USER";
+
+export const setSingleUser = (user) => {
+   return {
+       type: SET_SINGLE_USER,
+       user: user,
+   }
+}
+
+export const fetchSingleUser = (userId) => {
+    return async (dispatch) => {
+        response = await fetch(`api/user/${userId}`)
+        user = await response.json()
+        dispatch(setSingleUser(user))
+    }
+}
+
 export default reducer;
