e9c5d2b4f5595b38d3926943da42601cc61cc364
jimmy <demeatricej.sherrod@gmail.com>
2021-03-16 18:07:29 -0500

    creating skeleton in dreamcard

---
+++ react-app/src/components/DreamCard/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function DreamCard() {
+
+    return (
+        <div className="dream-card">
+            <div className="dream-title">
+            </div>
+            <div className="dream-keywords">
+            </div>
+            <div className="dream-notes">
+            </div>
+        </div>
+    )
+}
