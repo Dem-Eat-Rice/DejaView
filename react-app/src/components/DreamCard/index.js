5e56c2a50edbd7cab1b3097c95359c8773e2e5e3
jimmy <demeatricej.sherrod@gmail.com>
2021-03-24 23:45:27 -0500

    commenting out

--- react-app/src/components/DreamCard/index.js
+++ react-app/src/components/DreamCard/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
+// import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import fetchSingleUserDream from "../../store/users";
 
