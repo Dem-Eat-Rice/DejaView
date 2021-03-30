9c298ca0dd65142e7c5b859c7ccd88d0b87f1d9a
jimmy <demeatricej.sherrod@gmail.com>
2021-03-30 16:04:37 -0500

    adding starting stuff for footer

---
+++ react-app/src/components/Footer/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+import './Footer.css';
+
+const Footer = () => {
+
+    return (
+      <div className="footer-container">
+        <div className="footer-container_title">
+          Codezvous :
+          <br />
+          a <a href="http://meetup.com">Meetup</a> clone project
+        </div>
+          <div className="footer-container_jimmy">
+            <div className="name">Demeatrice James <br /> Sherrod</div>
+            <div className="icon-container">
+              <SocialIcon className="icon" url="https://github.com/Dem-Eat-Rice" />
+              <SocialIcon className="icon" url="https://www.linkedin.com/in/demeatrice-james-sherrod-2650091a4" />
+              <SocialIcon className="icon" url="mailto:demeatricej.sherrod@gmail.com" />
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default Footer;
