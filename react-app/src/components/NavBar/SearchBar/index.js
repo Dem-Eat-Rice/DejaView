8d23011286fc4cca504f39317e7536ad314c1cf2
jimmy <demeatricej.sherrod@gmail.com>
2021-03-31 09:05:52 -0500

    search text

--- react-app/src/components/NavBar/SearchBar/index.js
+++ react-app/src/components/NavBar/SearchBar/index.js
@@ -1,14 +1,18 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-import magnifyingGlass from "./icon4.png"
+import magnifyingGlass from "./icon4.png";
+import "./SearchBar.css";
 
 function SearchBar() {
+
+    const preventSearchRefreshOnClick = async (e) => {
+        e.preventDefault();
+    } 
 
     return (
         <div class="searchBar">
             <form>
                 <input className="search" type="text" placeholder="Search Dreams" />
-                <input type="image" alt="submit" id="glass" src={magnifyingGlass} />
+                <input type="image" alt="submit" onClick={preventSearchRefreshOnClick} id="glass" src={magnifyingGlass} />
             </form>
         </div>
     )
