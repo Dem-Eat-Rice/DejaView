6d31706ac51080b869ca3b83465580ec23e661bf
jimmy <demeatricej.sherrod@gmail.com>
2021-04-01 07:41:08 -0500

    im getting data back, not sure how to use it yet or where to use it exactly

--- react-app/src/components/NavBar/SearchBar/index.js
+++ react-app/src/components/NavBar/SearchBar/index.js
@@ -14,34 +14,46 @@ function SearchBar({ user }) {
         // loadDreams(searchInput);
         // history.push(`/users/${user.id}/dreams/9`)
     }
-    
-    const loadDreams = async(input) => {
+
+    const loadDreams = async (input) => {
         const response = await fetch(`/api/users/${user.id}/dreams`)
         const dreamsArray = await response.json();
         if (dreamsArray) {
             const newDream = dreamsArray.filter(dream => {
-               return dream.title.includes(input)
+                return (
+                    dream.title.includes(input) ||
+                    dream.notes.includes(input) ||
+                    dream.keywords.includes(input)
+                )
             })
-            return newDream
+            return await newDream
         }
         return null;
     }
 
-    const searchDropBox = loadDreams(searchInput)
-
+    
     return (
         <div class="searchBar">
             <form>
-                <input 
-                className="search" 
-                id="search-input" 
-                type="text" 
-                placeholder="Search Dreams..."
-                onChange={(e) => setSearchInput(e.target.value)} 
-                 />
-                 {searchDropBox.map(dream, key => {
-                     <div key={dream.id}>{dream.title}</div>
-                 })}
+                <input
+                    className="search"
+                    id="search-input"
+                    type="text"
+                    placeholder="Search Dreams..."
+                    onChange={async (e) => {
+                        setSearchInput(e.target.value);
+                        const searchDropBox = loadDreams(e.target.value);
+                        const thisInput = document.getElementById("search-input");
+                        console.log(thisInput, await searchDropBox)
+                        // if (!Array.isArray(searchDropBox)) {
+                        //     for (const dreams of searchDropBox) {
+                        //         return thisInput.append(dreams.title)        
+                        //     }
+                        // }
+                    }
+                }
+                />
+
                 <input type="image" alt="submit" onClick={preventSearchRefreshOnClick} id="glass" src={magnifyingGlass} />
             </form>
         </div>
