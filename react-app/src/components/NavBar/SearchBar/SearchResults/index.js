0e386858dbbaf5d1ffd8706a1ce22dcd8d14a3fa
jimmy <demeatricej.sherrod@gmail.com>
2021-04-03 23:55:33 -0500

    smoll com

--- react-app/src/components/NavBar/SearchBar/SearchResults/index.js
+++ react-app/src/components/NavBar/SearchBar/SearchResults/index.js
@@ -17,6 +17,7 @@ function SearchResults({ dream, user, setShowResults, setHiddenValue, setSearchV
         >\
         <Link to={`/users/${user.id}/dreams/${dream.id}`}>
             {dream.title}
+            
         </Link>
         </div>
     )
