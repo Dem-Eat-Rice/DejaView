fa2af3577cdceb08c43e225add743df83b2d73e2
jimmy <demeatricej.sherrod@gmail.com>
2021-04-05 19:40:30 -0500

    yeh

---
+++ react-app/src/components/SplashPage/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function SplashPage ({ authenticated, setAuthenticated}) {
+
+    return (
+        <div className="splash-page-container">
+            <div>
+            </div>
+        </div>
+    )
+}
+
+export default SplashPage;
