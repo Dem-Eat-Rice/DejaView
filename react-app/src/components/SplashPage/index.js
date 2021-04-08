0c68114e0df4496f7e9487a281b21901fcdd2ee3
jimmy <demeatricej.sherrod@gmail.com>
2021-04-08 13:08:35 -0500

    splash almsot there

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,13 +1,23 @@
 import React from "react";
 import "./SplashPage.css";
+import { useHistory } from "react-router-dom";
 
-function SplashPage ({ authenticated, setAuthenticated}) {
+function SplashPage () {
 
     return (
         <div className="splash-page-container">
             <div className="app-intro">
-            </div>
-            <div className="get-started">
+                <div className="intro">
+                    Have you ever had trouble remembering a dream?
+                <br />
+                <br />
+                Welcome to DejaView,
+                <br />
+                an app to help you record, organize, and
+                <br />
+                recall your dreams~
+                </div>
+                <div className="start-button">Let's get to it!</div>
             </div>
         </div>
     )
