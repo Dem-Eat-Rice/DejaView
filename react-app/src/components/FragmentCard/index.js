39a05d7bc4c0be0a1ac7632ec4d9bac6ba6e7ebf
jimmy <demeatricej.sherrod@gmail.com>
2021-04-08 20:22:53 -0500

    starting frag card

---
+++ react-app/src/components/FragmentCard/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function FragmentCard () {
+
+    return (
+        <div className="fragment-card_container">
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </div>
+    )
+}
