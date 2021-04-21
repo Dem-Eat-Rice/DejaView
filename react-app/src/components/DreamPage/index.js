54052deb4e3f389ad4c4b8db4972b93f9b3856a4
jimmy <demeatricej.sherrod@gmail.com>
2021-04-21 09:28:28 -0500

    uhoh

--- react-app/src/components/DreamPage/index.js
+++ react-app/src/components/DreamPage/index.js
@@ -2,11 +2,9 @@ import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import FragmentList from "../FragmentList";
 import { fetchDream } from "../../store/dreams";
 import { getDreamFragments } from "../../store/fragments";
-import DreamForm from "../DreamForm";
-import FragmentForm from "../FragmentForm";
-import DreamCard from "../DreamCard";
 import "./DreamPage.css";
 
 function DreamPage() {
@@ -21,7 +19,7 @@ function DreamPage() {
     const dreamFragments = useSelector(state => {
         return state.fragments
     });
-    
+
     useEffect(() => {
         dispatch(fetchDream(dreamId));
         dispatch(getDreamFragments(dreamId));
@@ -33,41 +31,44 @@ function DreamPage() {
             <h1> {currentDream.title}</h1>
             <div className="dream-header">
                 <div className="keywords_fragment-page">
-                    Reminders: 
+                    Reminders:
                     <br />
                     <br />
                     {currentDream.keywords}
                 </div>
                 <div className="notes_fragment-page">
                     <br />
+                    <br />
                     {currentDream.notes}
                 </div>
             </div>
             <div className="dream-body">
-                <div className="fragment-container">
-                    <div className="title-frag">
-                        {dreamFragments.title}
-                    </div>
-                    <div className="emotions-frag">
-                    </div>
-                    <div className="setting-frag">
-                    </div>
-                    <div className="description-frag">
-                    </div>
-                </div>
-                <div className="fragment-container">
-                    <div className="input-frag">
-                    </div>
-                    <div className="input-frag">
-                    </div>
-                </div>
-                <div className="fragment-container">
-                </div>
-                <div className="fragment-container">
-                </div>
-            </div>                         
+                <FragmentList fragments={dreamFragments} />
+            </div>
         </div>
     )
 }
 
 export default DreamPage;
+
+{/* 
+Starting here. For everything inside of "dreamFragments",
+I want to display a fragment-container
+inside of that fragment-container should be 
+    -A Form
+        -Each form will be composed of the input components
+
+    I would pass in the correct functions/state from the 
+    FragmentList(index.js) to each of the form input components...I think? 
+
+    for each fragment in the fragmentList
+    you'll create a 
+    <div fragment container>
+        <FragmentCard /> 
+    <div fragment container />
+    The FragmentCard will be a 
+
+    First, display the information
+    Second, make it draggable
+    Third, make it editable.
+*/}
